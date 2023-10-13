7af3266b9b95139c3a3e39677138378238d40a98
Darrenf040 <darrenfr83@gmail.com>
2023-10-12 19:11:25 -0500

    Reservation page styling

--- src/modules/reservation.js
+++ src/modules/reservation.js
@@ -4,25 +4,32 @@ function reservationSection(){
     const content = document.getElementById("content");
 
     const reserve = document.createElement("div");
+    reserve.className = "reservation-container"
     reserve.innerHTML = `
     <div class="reservation">
         <form action="">
-            <h1>Make A Reservation</h1>
-            <input type="date" id="date" />
-            <input type="time">
-            <input type="number" name="" id="" placeholder="How many People">
-            <button>Reserve Table</button>
+            <div class="form-container">
+                <h1>Make A Reservation</h1>
+                <input type="date" id="date" />
+                <input type="time">
+                <input type="number" name="" id="" placeholder="How many People">
+                <button type="submit">Reserve Table</button>
+            </div>
         </form>
-        <div>
-            <h1>located here</h1>
-            <h2>123456 elms street drive</h2>
-            <p>123-456-7890</p>
-            <p>mrbeast@gmail.com</p>
-            <h1>Hours of operation</h1>
-            <p>Monday-Saturday</p>
-            <p>6am to 2am</p>
-            <p>Sundays</p>
-            <p>Closed</p>
+        <div class="contact-container">
+        <h1>Contact Us</h1>
+            <div>
+                <h2>123456 elms street drive</h2>
+                <p>123-456-7890</p>
+                <p>mrbeast@gmail.com</p>
+            </div>
+            <div>
+                <h2>Hours of operation</h2>
+                <p>Monday-Saturday</p>
+                <p>6am to 2am</p>
+                <p>Sundays</p>
+                <p>Closed</p>
+            </div>
         </div>
     </div>`;    
 
@@ -33,5 +40,8 @@ export default function resrvationLoad(){
     const content = document.getElementById("content");
 
     content.innerHTML = '';
-    content.append(header(), reservationSection(), footer());
+    const wrapper = createElement("div");
+    wrapper.className = "reservation-section";
+    content.append(wrapper);
+    wrapper.append(header(), reservationSection(), footer());
 }
